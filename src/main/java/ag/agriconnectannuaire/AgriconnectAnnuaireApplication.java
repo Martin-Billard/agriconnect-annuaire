e17100a8f77422a5a391eb5042dbd2af8732b43d
Martin Billard <martin.billard@toulouse.miage.fr>
2024-05-28 13:53:34 +0200

    MECP-09 : Création de l'application

---
+++ src/main/java/ag/agriconnectannuaire/AgriconnectAnnuaireApplication.java
@@ -0,0 +1,16 @@
+package ag.agriconnectannuaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+
+public class AgriconnectAnnuaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriconnectAnnuaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ag/agriconnectannuaire/AgriconnectAnnuaireApplicationTests.java
@@ -0,0 +1,13 @@
+package ag.agriconnectannuaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriconnectAnnuaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
